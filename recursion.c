06c4d43d9c91fb3923a47e9d9f953cd66e8e1a0b
Anime1102 <123969568+Anime1102@users.noreply.github.com>
2023-01-31 08:37:19 -0800

    Add files via upload

---
+++ recursion.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int findFactorial(int n)
+{
+   if(n==1)
+       return 1;
+   else
+       return(n*findFactorial(n-1));
+ }
+int main()
+{
+  int n1,f;
+
+  printf(" Input  a number : ");
+  scanf("%d",&n1);
+  f=findFactorial(n1);
+  printf(" The Factorial of %d is : %d\n\n",n1,f);
+  return 0;
+}
+
